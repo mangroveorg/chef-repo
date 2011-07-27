9749ba8c381a54edc625e17ed39c9500f7b940ab
Asif Momin <asifmom@thoughtworks.com>
2011-07-27 16:09:58 +0530

    removing unnecessary stuff

--- cookbooks/mangroveapp/recipes/mangrove.rb
+++ cookbooks/mangroveapp/recipes/mangrove.rb
@@ -27,33 +27,6 @@ directory "/home/mangrover" do
 end
 
 
-# install sqlite3 using packages
-sqlite_packages = value_for_platform(
-    ["centos","redhat","fedora"] => 
-        {"default" => ["sqlite3", "sqlite"]},
-    "default" => 
-        ["sqlite3", "sqlite"]
-  )
-
-sqlite_packages.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
-
-# install couchdb using packages
-couchdb_packages = value_for_platform(
-    ["centos","redhat","fedora"] => 
-        {"default" => ["couchdb"]},
-    "default" => 
-        ["couchdb"]
-  )
-
-couchdb_packages.each do |pkg|
-  package pkg do
-    action :install
-  end
-end
 
 # install scms using packages
 scm_packages = value_for_platform(
